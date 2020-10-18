a5e9fdc49cc2f523594b3052dc6a077fd89a4064
MontoyaAndres <andresmontoyafcb@gmail.com>
2020-10-17 22:13:58 -0500

    sessions lambdas with serverless framework

---
+++ session-store-node/handlers/createSession.js
@@ -0,0 +1,74 @@
+const DynamoDB = require('aws-sdk/clients/dynamodb');
+const { v4: uuidv4 } = require('uuid');
+
+const { makeHandler } = require('./utils');
+
+const dynamodb = new DynamoDB.DocumentClient();
+
+const EXPIRATION = 7 * 86400000;
+
+const inputSchema = {
+  type: 'object',
+  properties: {
+    body: {
+      type: 'object',
+      properties: {
+        username: {
+          type: 'string',
+        },
+        password: {
+          type: 'string',
+        },
+      },
+      required: ['username', 'password'],
+    },
+  },
+};
+
+const handler = async event => {
+  const { username } = event.body;
+  const sessionId = uuidv4();
+  const createdAt = new Date();
+  const expiresAt = new Date(
+    createdAt.setTime(createdAt.getDate() + EXPIRATION)
+  );
+
+  try {
+    await dynamodb
+      .put({
+        TableName: process.env.TABLE_NAME,
+        Item: {
+          SessionId: sessionId,
+          Username: username,
+          CreatedAt: createdAt.toISOString(),
+          ExpiresAt: expiresAt.toISOString(),
+          TTL: (expiresAt.getTime() / 1000).toString(),
+        },
+        ConditionExpression: 'attribute_not_exists(SessionId)',
+      })
+      .promise();
+
+    return {
+      statusCode: 200,
+      body: JSON.stringify({
+        sessionId,
+        error: null,
+      }),
+    };
+  } catch (error) {
+    if (error.code === 'ConditionalCheckFailedException') {
+      // This is where you would put error handling logic on a condition expression failure
+      console.log('Holy moley -- a UUID collision!');
+    }
+
+    return {
+      statusCode: 500,
+      body: JSON.stringify({
+        sessionId: null,
+        error: 'Could not create session token',
+      }),
+    };
+  }
+};
+
+exports.handler = makeHandler({ handler, inputSchema });
---
+++ session-store-node/handlers/getSession.js
@@ -0,0 +1,55 @@
+const DynamoDB = require('aws-sdk/clients/dynamodb');
+
+const { makeHandler } = require('./utils');
+
+const dynamodb = new DynamoDB.DocumentClient();
+
+const inputSchema = {
+  type: 'object',
+  properties: {
+    headers: {
+      type: 'object',
+      properties: {
+        Authorization: { type: 'string' },
+      },
+      required: ['Authorization'],
+    },
+  },
+  required: ['headers'],
+};
+
+const handler = async event => {
+  const sessionId = event.headers.Authorization.toLowerCase().replace(
+    'bearer',
+    ''
+  );
+
+  const response = await dynamodb
+    .get({
+      TableName: process.env.TABLE_NAME,
+      Key: {
+        SessionId: sessionId,
+      },
+    })
+    .promise();
+
+  if (typeof response.Item === 'undefined') {
+    return {
+      statusCode: 401,
+      body: JSON.stringify({
+        username: null,
+        error: 'Session does not exist.',
+      }),
+    };
+  }
+
+  return {
+    statusCode: 200,
+    body: JSON.stringify({
+      username: response.Item.Username,
+      error: null,
+    }),
+  };
+};
+
+exports.handler = makeHandler({ handler, inputSchema });
