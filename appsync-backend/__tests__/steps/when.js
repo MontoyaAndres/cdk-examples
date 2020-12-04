f0918029e1e24d2a1fc609979f92771c0d553e14
MontoyaAndres <andresmontoyafcb@gmail.com>
2020-12-04 12:16:19 -0500

    added tests for unlike mutation

--- appsync-backend/__tests__/steps/when.js
+++ appsync-backend/__tests__/steps/when.js
@@ -370,6 +370,24 @@ const a_user_calls_like = async (user, tweetId) => {
   return result;
 };
 
+const a_user_calls_unlike = async (user, tweetId) => {
+  const unlike = `
+    mutation unlike($tweetId: ID!) {
+      unlike(tweetId: $tweetId)
+    }
+  `;
+  const variables = {
+    tweetId,
+  };
+
+  const data = await GraphQL(API_URL, unlike, variables, user.accessToken);
+  const result = data.unlike;
+
+  console.log(`[${user.username}] - unliked tweet [${tweetId}]`);
+
+  return result;
+};
+
 module.exports = {
   we_invoke_confirmUserSignup,
   a_user_signs_up,
@@ -383,4 +401,5 @@ module.exports = {
   a_user_calls_getTweets,
   a_user_calls_getMyTimeline,
   a_user_calls_like,
+  a_user_calls_unlike,
 };
